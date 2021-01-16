57d2819723327173260fe742a6956a3d536fabe5
Ray <raitis.cerkasovs@gmail.com>
2021-01-16 20:55:14 +0000

    fix user profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,18 @@
 import React, { Component }  from 'react'
 import Layout from '../components/Layout'
 import UserProfile from '../components/UserProfile'
+import { Auth0Provider } from "@auth0/auth0-react"; 
 
 export default () => {
     return (
         <Layout>
-            <UserProfile />
+            <Auth0Provider
+                domain={process.env.NEXT_PUBLIC_auth0_domain}
+                clientId={process.env.NEXT_PUBLIC_auth0_clientId_dev}
+                redirectUri={process.env.NEXT_PUBLIC_auth0_redirectUri}
+            >
+                <UserProfile />
+            </Auth0Provider>
         </ Layout>
     );
 }
